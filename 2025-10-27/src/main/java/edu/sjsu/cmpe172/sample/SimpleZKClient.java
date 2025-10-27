d16b300e0bc522cd2a337c89a3bf626d1b535462
ben reed <ben.reed@sjsu.edu>
2025-10-27 00:00:32 -0700

    in-class example for 10-27-2025

---
+++ 2025-10-27/src/main/java/edu/sjsu/cmpe172/sample/SimpleZKClient.java
@@ -0,0 +1,77 @@
+package edu.sjsu.cmpe172.sample;
+
+import org.apache.zookeeper.*;
+import picocli.CommandLine;
+
+import java.io.IOException;
+
+@CommandLine.Command(name = "SimpleZKClient")
+public class SimpleZKClient implements Runnable {
+    @CommandLine.Parameters
+    String connectString;
+
+    @CommandLine.Option(names = "--description", defaultValue = "anonymous")
+    String description;
+    private ZooKeeper zk;
+
+    public static void main(String[] args) {
+        System.exit(new CommandLine(new SimpleZKClient()).execute(args));
+    }
+
+    Watcher childrenWatcher = e -> {
+        if (e.getType() == Watcher.Event.EventType.NodeChildrenChanged) {
+            System.out.println("Children changed! Children are now:");
+            try {
+                var children = zk.getChildren("/", this.childrenWatcher);
+                children.forEach(child -> {
+                    try {
+                        byte[] data = zk.getData("/" + child, false, null);
+                        if (data == null) {
+                            data = ("no data for /" + child).getBytes();
+                        }
+                        System.out.println("    " + child + ": " + new String(data));
+                    } catch (KeeperException | InterruptedException ex) {
+                        System.out.println("    " + child + ": " + ex.getMessage());
+                    }
+                });
+            } catch (KeeperException|InterruptedException ex) {
+                System.out.println("Got exception trying to get children: " + ex.getMessage());
+            }
+
+        }
+    };
+
+    @Override
+    public void run() {
+        System.out.println("Using connectString " + connectString);
+        try {
+            zk = new ZooKeeper(connectString, 10000, e -> {
+               if (isFatal(e.getState())) {
+                   System.out.println("ZooKeeper session failed: " + e);
+                   System.exit(2);
+               }
+            });
+            var name = zk.create("/simple-", description.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+            System.out.println("I am " + name);
+
+            System.out.println("Trying to become the boss!");
+            zk.create("/boss", name.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+
+            childrenWatcher.process(new WatchedEvent(Watcher.Event.EventType.NodeChildrenChanged, null, "/"));
+            for (int i = 0; i < 10; i++) {
+                zk.setData(name, (description + " is bored... " + i).getBytes(), -1);
+                Thread.sleep(10000);
+            }
+        } catch (IOException | InterruptedException | KeeperException e) {
+            System.out.println("ZooKeeper session create failed: " + e);
+            System.exit(2);
+        }
+    }
+
+    private static boolean isFatal(Watcher.Event.KeeperState state) {
+        return switch (state) {
+            case Closed, Expired, AuthFailed -> true;
+            default -> false;
+        };
+    }
+}
